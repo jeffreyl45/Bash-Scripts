6b947dfcc9f5899a925aaabfed6c3eeeb9016fb8
Jeffrey Lu <111028341+jeffreyl45@users.noreply.github.com>
2024-10-01 20:18:09 -0400

    Update adder2.sh

--- adder2.sh
+++ adder2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sum=0
 count="$#"
-for ((i = 1; i <= $count; i ++))
+# $# is the number of arguments passed in after the file name
+for ((i = 1; i <= $count; i ++)) 
+# add 1 + 2 + 3 +... + number of arguments passed in
 do
     ((sum+=$i))
 done

e052b4292aea5ade6dd2e42b7c0f3227e951b84a
Jeffrey Lu <111028341+jeffreyl45@users.noreply.github.com>
2024-10-01 20:23:51 -0400

    Update expressions.sh

--- expressions.sh
+++ expressions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RESULT1=$(( 1 + 5 ))
+# double bracket allows you to do math instead of just a string
 RESULT2=$(expr 1 + 5) # both accomplish the same thing
 
 echo "RESULT1: $RESULT1"
